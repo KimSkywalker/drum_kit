eefb8a96b7ddfde09207397ca91534135c146ba8
KimSkywalker <64174241+KimSkywalker@users.noreply.github.com>
2020-05-21 17:06:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+let buttonArray = document.querySelectorAll(".drum");
+let numberOfButtons = document.querySelectorAll(".drum").length;
+
+for( let i = 0; i < numberOfButtons; i++){
+  buttonArray[i].addEventListener("click", handleClick);
+
+}
+
+function handleClick() {
+  let hitButton = this.innerHTML;
+  makeSound(hitButton);
+}
+
+addEventListener("keydown", getKey);
+
+function getKey (event) {
+    makeSound(event.key);
+  }
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+    audio = new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+
+    case "a":
+    audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+
+    case "s":
+    audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+
+    case "d":
+    audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+    case "j":
+    audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+
+    case "k":
+    audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+
+    case "l":
+    audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+
+    default: console.log(key);
+  }
+}
